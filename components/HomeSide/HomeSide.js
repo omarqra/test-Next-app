aaf105715c5e518e6673f11274bcf40afaedbafc
omarqra <ammarqm5@gmail.com>
2021-09-25 10:10:33 +0300

    delete moaz logs

--- components/HomeSide/HomeSide.js
+++ components/HomeSide/HomeSide.js
@@ -4,39 +4,55 @@ import image_1 from "../../public/facebook.png";
 import image_2 from "../../public/instagram.jpg";
 import image_3 from "../../public/twitter.jpg";
 const HomeSide = () => {
-    console.log(image_1);
-    return (
-        <div className={classes.homeSide}>
-            <ul className={classes.socialList}>
-                <li>
-                    <img src={image_1.src} alt="facebook" />
-                    <a href="https://www.facebook.com/" target="_blank"> @المطرجي</a>
-                </li>
-                <li>
-                    <img src={image_3.src} alt="facebook" />
-                    <a href="https://www.twitter.com/" target="_blank"> @موقع المطرجي </a>
-                </li>
-                <li>
-                    <img src={image_2.src} alt="facebook" />
-                    <a href="https://www.instagram.com/" target="_blank"> mutraji221@ </a>
-                </li>
-            </ul>
-            <ul className={classes.linksList}>
-                <li>
-                    <Link href="/"><a className={classes.link}>من نحن</a></Link>
-                </li>
-                <li>
-                    <Link href="/"><a className={classes.link}>سياسة الخصوصية</a></Link>
-                </li>
-                <li>
-                    <Link href="/"><a className={classes.link}>شروط الاستخدام</a></Link>
-                </li>
-                <li>
-                    <Link href="/"><a className={classes.link}>اتصل بنا</a></Link>
-                </li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className={classes.homeSide}>
+      <ul className={classes.socialList}>
+        <li>
+          <img src={image_1.src} alt="facebook" />
+          <a href="https://www.facebook.com/" target="_blank">
+            {" "}
+            @المطرجي
+          </a>
+        </li>
+        <li>
+          <img src={image_3.src} alt="facebook" />
+          <a href="https://www.twitter.com/" target="_blank">
+            {" "}
+            @موقع المطرجي{" "}
+          </a>
+        </li>
+        <li>
+          <img src={image_2.src} alt="facebook" />
+          <a href="https://www.instagram.com/" target="_blank">
+            {" "}
+            mutraji221@{" "}
+          </a>
+        </li>
+      </ul>
+      <ul className={classes.linksList}>
+        <li>
+          <Link href="/">
+            <a className={classes.link}>من نحن</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a className={classes.link}>سياسة الخصوصية</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a className={classes.link}>شروط الاستخدام</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a className={classes.link}>اتصل بنا</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default HomeSide;
--- pages/index.js
+++ pages/index.js
@@ -3,44 +3,51 @@ import HomeSide from "../components/HomeSide/HomeSide";
 import TopicsList from "../components/Topics/TopicsList";
 import classes from "../styles/homePage.module.css";
 const StaticTopics = [
-    {
-        id: 1,
-        imgSrc: "https://i.ytimg.com/vi/GRA89joKjp4/maxresdefault.jpg",
-        title: "أفضل أنواع نبات الظل",
-    },
-    {
-        id: 2,
-        imgSrc: "https://i.ytimg.com/vi/GRA89joKjp4/maxresdefault.jpg",
-        title: "أغرب الكائنات البحرية في العالم",
-    },
-    {
-        id: 3,
-        imgSrc: "https://i.ytimg.com/vi/GRA89joKjp4/maxresdefault.jpg",
-        title: "فالبروات الصودوم valproate Sodiur الأدوية المضادة للصرع",
-    },
-    {
-        id: 4,
-        imgSrc: "https://i.ytimg.com/vi/GRA89joKjp4/maxresdefault.jpg",
-        title: "فالبروات الصودوم valproate Sodiur الأدوية المضادة للصرع",
-    }
-]
+  {
+    id: 1,
+    imgSrc: "https://i.ytimg.com/vi/GRA89joKjp4/maxresdefault.jpg",
+    title: "أفضل أنواع نبات الظل",
+  },
+  {
+    id: 2,
+    imgSrc: "https://i.ytimg.com/vi/GRA89joKjp4/maxresdefault.jpg",
+    title: "أغرب الكائنات البحرية في العالم",
+  },
+  {
+    id: 3,
+    imgSrc: "https://i.ytimg.com/vi/GRA89joKjp4/maxresdefault.jpg",
+    title: "فالبروات الصودوم valproate Sodiur الأدوية المضادة للصرع",
+  },
+  {
+    id: 4,
+    imgSrc: "https://i.ytimg.com/vi/GRA89joKjp4/maxresdefault.jpg",
+    title: "فالبروات الصودوم valproate Sodiur الأدوية المضادة للصرع",
+  },
+];
 
 const Home = () => {
-    console.log(StaticTopics[0].imgSrc);
-    return (
+  return (
     <div>
-        <div className={classes.homePackgroundDiv}>
-            <img className={classes.homePackground} src="http://www.hdwallpaperspulse.com/wp-content/uploads/2015/05/30/art-desktop-luxury-wallpapers.jpg" alt="home image" />
-            <p className={classes.PrgOnImg}>اعرف أكثر عن المشروع</p>
-        </div>
-        <Link href="/"><a className={classes.detailsLinks}>الأكثر زيارة</a></Link>
-        <div className={classes.homeSideAndTopics}>
-            <TopicsList topics={StaticTopics}/>
-            <HomeSide />
-        </div>
-        <Link href="/"><a className={classes.detailsLinks}>المزيد</a></Link>
+      <div className={classes.homePackgroundDiv}>
+        <img
+          className={classes.homePackground}
+          src="http://www.hdwallpaperspulse.com/wp-content/uploads/2015/05/30/art-desktop-luxury-wallpapers.jpg"
+          alt="home image"
+        />
+        <p className={classes.PrgOnImg}>اعرف أكثر عن المشروع</p>
+      </div>
+      <Link href="/">
+        <a className={classes.detailsLinks}>الأكثر زيارة</a>
+      </Link>
+      <div className={classes.homeSideAndTopics}>
+        <TopicsList topics={StaticTopics} />
+        <HomeSide />
+      </div>
+      <Link href="/">
+        <a className={classes.detailsLinks}>المزيد</a>
+      </Link>
     </div>
-    )
-}
+  );
+};
 
 export default Home;
