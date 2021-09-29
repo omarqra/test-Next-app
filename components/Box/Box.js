5d12b7ad5bd2bf359c587d437e760ae8ada93c47
moazqramohammed <qrhmhmdmadh@gmail.com>
2021-09-29 13:00:51 +0300

    finished about page and Update Footer

--- components/Box/Box.js
+++ components/Box/Box.js
@@ -3,17 +3,24 @@ const Box = ({title, text}) => {
     let state = false;
     const fixClick = (e) => {
         state = !state;
-        if (state) e.target.nextSibling.className = classes.boxTextShow;
-        else e.target.nextSibling.className = classes.boxTextUnShow;
+        if (state) {
+            e.target.nextSibling.style.height = `${e.target.nextSibling.firstChild.clientHeight}px`;
+        }
+        else {
+            e.target.nextSibling.style.height = "0";
+        }
+
     }
     return (
         <div className={classes.box} onClick={fixClick}>
             <h3 className={classes.boxTitle}>
                     {title}
             </h3>
-            <p className={classes.boxTextUnShow}>
-                {text}
-            </p>
+            <div className={classes.boxTextShow}>
+                <p>
+                    {text}
+                </p>
+            </div>
         </div>
     )
 }
